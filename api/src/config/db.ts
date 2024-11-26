6c47cd1e7c44adc67db49e0aaaf8af180cc852ee
AhmedFatthy1040 <eltonsy1040@outlook.sa>
2024-11-27 00:36:32 +0200

    chore(config): update db.ts to use environment variable for DB host

--- api/src/config/db.ts
+++ api/src/config/db.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 const pool = new Pool({
   user: process.env.DB_USER,
-  host: "localhost", // Container name if using Docker
+  host: process.env.DB_HOST, // Container name if using Docker
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : undefined,
